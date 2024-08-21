5b7bf2978765266e03841e16448b9050b5571720
Cydral <53169060+Cydral@users.noreply.github.com>
2024-08-21 22:36:48 +0200

    Add files via upload

--- sources/vslm.cpp
+++ sources/vslm.cpp
@@ -2090,7 +2090,7 @@ Be all my sins remembered.)";
                 trainer_a.set_mini_batch_size(mini_batch_size);
                 trainer_a.be_verbose();
                 trainer_a.set_iterations_without_progress_threshold(2500);
-                for (int epoch = 0; epoch < num_epochs && trainer_a.get_learning_rate() < trainer_a.get_min_learning_rate() && !g_interrupt_signal_received; ++epoch) {
+                for (int epoch = 0; epoch < num_epochs && trainer_a.get_learning_rate() > trainer_a.get_min_learning_rate() && !g_interrupt_signal_received; ++epoch) {
                     for (size_t i = 0; i < batches.size(); ++i) trainer_a.train_one_step(batches[i], label_batches[i]);
                 }
                 trainer_a.get_net();
@@ -2115,7 +2115,7 @@ Be all my sins remembered.)";
                 trainer_b.set_mini_batch_size(mini_batch_size);
                 trainer_b.be_verbose();
                 trainer_b.set_iterations_without_progress_threshold(500);
-                for (int epoch = 0; epoch < num_epochs && trainer_b.get_learning_rate() < trainer_b.get_min_learning_rate() && !g_interrupt_signal_received; ++epoch) {
+                for (int epoch = 0; epoch < num_epochs && trainer_b.get_learning_rate() > trainer_b.get_min_learning_rate() && !g_interrupt_signal_received; ++epoch) {
                     for (size_t i = 0; i < batches.size(); ++i) trainer_b.train_one_step(batches[i], label_batches[i]);
                 }
                 trainer_b.get_net();
