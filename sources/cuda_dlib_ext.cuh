3c54e900fc8a91211e184f06d0c5f691c002ff1f
Cydral <53169060+Cydral@users.noreply.github.com>
2024-08-25 15:51:08 +0200

    Add files via upload

---
+++ sources/cuda_dlib_ext.cuh
@@ -0,0 +1,36 @@
+#ifndef DLIB_DNN_CuDA_EXT_H_
+#define DLIB_DNN_CuDA_EXT_H_
+
+#include <dlib/cuda/cuda_utils.h>
+#include <dlib/cuda/cuda_dlib.h>
+#include <dlib/cuda/cudnn_dlibapi.h>
+#include <math_constants.h>
+
+namespace dlib
+{
+    namespace cuda
+    {
+#ifdef DLIB_USE_CUDA
+        void rms_normalize(
+            const double eps,
+            resizable_tensor& dest,
+            resizable_tensor& scale,
+            const tensor& src,
+            const tensor& gamma
+        );
+
+        void rms_normalize_gradient(
+            const double eps,
+            const tensor& gradient_input,
+            const tensor& scale,
+            const tensor& src,
+            const tensor& gamma,
+            tensor& src_grad,
+            tensor& gamma_grad
+        );
+#else // if DLIB_USE_CUDA NOT DEFINED
+#endif // DLIB_USE_CUDA
+    }
+}
+
+#endif // DLIB_DNN_CuDA_EXT_H_
